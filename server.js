f323ae85adf0dd90662502d560ee3e6081dd96e1
BobaFed <jori.immo@gmail.com>
2023-02-13 12:59:16 +0200

    bug fixes

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const express = require("exrpess");
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 
 app.get("/", function(request, respond) {
     // server respond 
-    respond.sendFile(__dirname + "index.html");
+    respond.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000, function () {

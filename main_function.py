a0b22f7d8a9c7c654bd17abca8fb6797b9d01876
Huaptlo <114053742+Huaptlo@users.noreply.github.com>
2023-02-16 11:31:49 +0200

    Create main_function.py

---
+++ main_function.py
@@ -0,0 +1,11 @@
+# Initiative tracker project 2023
+# Main code functionality
+# Written by Max Creutz
+
+class Initiative_tracker:
+    def __init__(self):
+        pass
+
+    initiative_list = []
+    initiative_list_ordered = []
+    

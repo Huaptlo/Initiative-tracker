05a120ff04a62da719f798b5f9ecade0c3f0dc76
Huaptlo <114053742+Huaptlo@users.noreply.github.com>
2023-02-20 12:36:12 +0200

    Update function_version.py

--- function_version.py
+++ function_version.py
@@ -1,21 +1,21 @@
 name_value_list = []
 
 def add_name_value(name, value):
-    """Adds a name-value pair to the global list"""
+    # Lisää nimen ja arvon listaan
     name_value_list.append((name, value))
 
 def sort_names_values():
-    """Sorts the global list by value in descending order"""
+    # Järjestää nimet listassa takaperin
     name_value_list.sort(key=lambda x: x[1], reverse=True)
 
 def print_highest_value(current_index):
-    """Prints the current highest name and value to the console"""
+    # Tulostaa ensimmäisen listassa olevan nimen ja arvon
     current_name, current_value = name_value_list[current_index]
     print(f"Highest name: {current_name}, value: {current_value}")
 
-# example usage of the functions:
-add_name_value('Alice', 10)
-add_name_value('Bob', 5)
-add_name_value('Charlie', 20)
+# Esimerkkejä käytöstä
+add_name_value('Pekka', 10)
+add_name_value('Jukka', 5)
+add_name_value('Örkki 1', 20)
 sort_names_values()
-print_highest_value(0) # prints "Highest name: Charlie, value: 20"
+print_highest_value(0) # Tulostaa "Highest name: Charlie, value: 20"

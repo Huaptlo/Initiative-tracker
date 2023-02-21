171f8fb65dc7319237d49a6b37831b2a754d66f9
BobaFed <jori.immo@gmail.com>
2023-02-21 13:15:18 +0200

    add character makes a list

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
+//importataan moduulit
 const express = require("express");
-const bodyParser = require("body-parser");
 
+// otetaan käyttöön
 const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
 
+// käytetään ejs tiedostoja html sijaan
+app.set('view engine', 'ejs');
 
+// aktivoidaan body parser ja staattiset sivut
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
 
-app.get("/", function(request, respond) {
-    // server respond 
-    respond.sendFile(__dirname + "/index.html");
+let chrList = [];
+
+app.get("/", function(req, res) {
+    // server respond
+    res.render("index", {characterList: chrList});
+});
+
+app.post("/", function(req, res) {
+    let chrName = req.body.chrName;
+    let chrInit = req.body.chrIniti;
+
+    // luodaan hahmo objecti
+    let newCharacter = {
+        name: chrName,
+        initiative: chrInit
+    };
+
+    chrList = chrList.sort();
+    chrList.push(newCharacter);
+    res.redirect("/");
 });
 
 app.listen(3000, function () {

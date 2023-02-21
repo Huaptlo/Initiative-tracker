1c19ff75ab768db0b517debcea3303b56bff1fa0
Huaptlo <114053742+Huaptlo@users.noreply.github.com>
2023-02-21 08:58:54 +0200

    Update function_version.py

--- function_version.py
+++ function_version.py
@@ -14,8 +14,11 @@ def print_highest_value(current_index):
     print(f"Nimi: {current_name}, arvo: {current_value}")
 
 # Esimerkkejä käytöstä
+# Lisätään nimi ja arvo
 add_name_value('Pekka', 10)
 add_name_value('Jukka', 5)
 add_name_value('Örkki 1', 20)
+# Järjestetään lista
 sort_names_values()
+# Haetaan listasta nesimmäinen arvo, muuttamalla suluissa olevaa lukua saadaan haettua eri nimi ja arvo
 print_highest_value(0) # Tulostaa "Nimi: Örkki 1, arvo: 20"

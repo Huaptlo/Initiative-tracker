8c7b7666ddc13cebfebfd0b071507e145882a307
Huaptlo <114053742+Huaptlo@users.noreply.github.com>
2023-02-21 09:04:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ name_value_list = []
 # Looppi joka pyytää nimeä ja arvoa jotka lisätään listaan
 while True:
     # Kysytään nimeä ja arvoa
-    name = input("Name (or 'end' to exit): ")
-    if name == 'end':
+    name = input("Nimi (tai 'Q' lopettaaksesi): ")
+    if name == 'Q':
         break
-    value = int(input("Initiative: "))
+    value = int(input("Aloite: "))
 
     # Lisätään nimi ja arvo listaan
     name_value_list.append((name, value))
@@ -23,8 +23,8 @@ name_value_list.sort(key=lambda x: x[1], reverse=True)
 current_index = 0
 while True:
     current_name, current_value = name_value_list[current_index]
-    print(f"Highest name: {current_name}, value: {current_value}")
-    input_str = input("Press enter for next (or type 'end' to exit): ")
-    if input_str == 'end':
+    print(f"Nimi: {current_name}, aloite: {current_value}")
+    input_str = input("Paina enter seuraavaan (tai 'Q' lopettaaksesi): ")
+    if input_str == 'Q':
         break
     current_index = (current_index + 1) % len(name_value_list)

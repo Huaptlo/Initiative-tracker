5500f3d381d42c2b35a932c4dc3a1f405f2d3306
Huaptlo <114053742+Huaptlo@users.noreply.github.com>
2023-02-23 09:46:33 +0200

    Update UI_App.py

--- UI_App.py
+++ UI_App.py
@@ -1,6 +1,6 @@
 import kivy
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
 from kivy.core.window import Window
 from kivy.uix.textinput import TextInput
@@ -8,29 +8,29 @@ from kivy.uix.label import Label
 kivy.require('2.1.0')
 
 
-class MyBoxLayout(BoxLayout):
+class MyFloatLayout(FloatLayout):
     
     def __init__(self, **kwargs):
-        super(MyBoxLayout, self).__init__(**kwargs)
+        super(MyFloatLayout, self).__init__(**kwargs)
         
         # Create input fields for name and value
-        self.name_input = TextInput(hint_text='Enter Name')
+        self.name_input = TextInput(hint_text='Enter Name', size_hint=(0.8, 0.1), pos_hint={'x': 0.1, 'y': 0.8})
         self.add_widget(self.name_input)
         
-        self.value_input = TextInput(hint_text='Enter Value')
+        self.value_input = TextInput(hint_text='Enter Value', size_hint=(0.8, 0.1), pos_hint={'x': 0.1, 'y': 0.6})
         self.add_widget(self.value_input)
         
         # Create a button to add name and value to list
-        self.add_button = Button(text='Add to List')
+        self.add_button = Button(text='Add to List', size_hint=(0.4, 0.1), pos_hint={'x': 0.3, 'y': 0.4})
         self.add_button.bind(on_press=self.add_to_list)
         self.add_widget(self.add_button)
         
         # Create a label to display the list of name-value pairs
-        self.label = Label(text='', size_hint_y=None, height=Window.height)
+        self.label = Label(text='', size_hint=(0.8, 0.4), pos_hint={'x': 0.1, 'y': 0})
         self.add_widget(self.label)
         
         # Create a button to close the app
-        self.close_button = Button(text='X', size_hint=(None, None), size=(50, 50), pos_hint={'x': 0.95, 'y': 0.95})
+        self.close_button = Button(text='X', size_hint=(0.1, 0.1), pos_hint={'x': 0.9, 'y': 0.9})
         self.close_button.bind(on_press=self.close_app)
         self.add_widget(self.close_button)
         
@@ -59,10 +59,10 @@ class MyApp(App):
     
     def build(self):
         # Set the app to full-screen
-        Window.fullscreen = True
+        # Window.fullscreen = True
         
         # Create a layout for the app
-        layout = MyBoxLayout(orientation='vertical')
+        layout = MyFloatLayout()
         
         return layout
     
